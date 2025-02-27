374723839f6ed98669662d79fa4dbe7e55f7cf79
coladonz <coladonzwtf@gmail.com>
2025-02-28 00:15:53 +1000

    feat: add tangu hook test

--- src/TanguHook.sol
+++ src/TanguHook.sol
@@ -1,6 +1,170 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.24;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import "v4-core/src/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import "v4-core/src/types/BeforeSwapDelta.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IAavePool} from "./interfaces/IAavePool.sol";
+import "forge-std/console2.sol";
 
-contract TanguHook {}
+contract TanguHook is BaseHook, Ownable {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using BeforeSwapDeltaLibrary for BeforeSwapDelta;
+    using SafeERC20 for IERC20;
+
+    Currency public immutable USDC = Currency.wrap(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    Currency public immutable USDT = Currency.wrap(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+
+    IAavePool public immutable aavePool;
+
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    mapping(Currency => uint256) public devFeeAccrued;
+    mapping(Currency => uint256) public totalFeeAccrued;
+
+    mapping(Currency => IERC20) public aTokens;
+    mapping(Currency => uint256) public totalATokenShares;
+
+    mapping(Currency => mapping(address => UserInfo)) public userInfos;
+    mapping(Currency => uint256) public rewardPerShare;
+    mapping(Currency => uint256) public totalShares;
+
+    // TODO: Add events
+    error InvalidPoolTokens();
+
+    constructor(IPoolManager _poolManager, IAavePool _pool) BaseHook(_poolManager) Ownable(msg.sender) {
+        aavePool = _pool;
+
+        IERC20(Currency.unwrap(USDC)).forceApprove(address(aavePool), type(uint256).max);
+        IERC20(Currency.unwrap(USDT)).forceApprove(address(aavePool), type(uint256).max);
+    }
+
+    function setATokens(address aToken, Currency uToken) external onlyOwner {
+        aTokens[uToken] = IERC20(aToken);
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function pendingRewards(address user, Currency currency) public view returns (uint256) {
+        UserInfo storage userInfo = userInfos[currency][user];
+        uint256 reward = (userInfo.amount * rewardPerShare[currency]) / 1e18 - userInfo.rewardDebt;
+
+        return (reward * _getATokenSharePrice(currency)) / 1e18;
+    }
+
+    function claimFee(address user, Currency currency) external {
+        _claimFee(user, currency);
+    }
+
+    function claimDevFee(Currency currency) external onlyOwner {
+        _withdrawAave(devFeeAccrued[currency], address(this), currency);
+    }
+
+    function _addRewards(Currency currency, uint256 amount) internal {
+        rewardPerShare[currency] += (amount * 1e18) / totalShares[currency];
+    }
+
+    function _deposit(address user, uint amount, Currency currency) internal {
+        UserInfo storage userInfo = userInfos[currency][user];
+        userInfo.amount += amount;
+        userInfo.rewardDebt += (amount * rewardPerShare[currency]) / 1e18;
+        totalShares[currency] += amount;
+    }
+
+    function _claimFee(address user, Currency currency) internal {
+        uint pendingReward = pendingRewards(user, currency);
+        UserInfo storage userInfo = userInfos[currency][user];
+        if (pendingReward > 0) {
+            userInfo.rewardDebt = (userInfo.amount * rewardPerShare[currency]) / 1e18;
+            totalShares[currency] -= userInfo.amount;
+            userInfo.amount = 0;
+            _withdrawAave(pendingReward, user, currency);
+        }
+    }
+
+    function _getATokenSharePrice(Currency currency) internal view returns (uint) {
+        return (aTokens[currency].balanceOf(address(this)) * 1e18) / totalATokenShares[currency];
+    }
+
+    function _depositAave(uint share, Currency currency) internal {
+        totalATokenShares[currency] += share;
+        aavePool.supply(Currency.unwrap(currency), share, address(this), 0);
+    }
+
+    function _withdrawAave(uint share, address to, Currency currency) internal returns (uint) {
+        uint amount = (share * _getATokenSharePrice(currency)) / 1e18;
+        totalATokenShares[currency] -= share;
+        return aavePool.withdraw(Currency.unwrap(currency), amount, to);
+    }
+
+    // -----------------------------------------------
+    // NOTE: see IHooks.sol for function documentation
+    // -----------------------------------------------
+
+    function _beforeInitialize(address, PoolKey calldata key, uint160) internal override returns (bytes4) {
+        if (equals(key.currency0, USDC) && equals(key.currency1, USDT)) {
+            return BaseHook.beforeInitialize.selector;
+        }
+
+        revert InvalidPoolTokens();
+    }
+
+    function _beforeSwap(
+        address sender,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata swapParams,
+        bytes calldata
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
+        uint swapAmount = uint256(
+            swapParams.amountSpecified > 0 ? swapParams.amountSpecified : -swapParams.amountSpecified
+        );
+        uint fee = swapAmount / 1000;
+        Currency swapCurrency = swapParams.zeroForOne ? USDC : USDT;
+
+        // Take Fee
+        poolManager.take(swapCurrency, address(this), fee);
+        _depositAave(fee, swapCurrency);
+
+        // Update Fee, Deposit
+        totalFeeAccrued[swapCurrency] += fee;
+        devFeeAccrued[swapCurrency] += fee / 2;
+        _deposit(sender, swapAmount, swapCurrency);
+        _addRewards(swapParams.zeroForOne ? USDC : USDT, fee / 2);
+
+        // Return Delta
+        BeforeSwapDelta delta = toBeforeSwapDelta(int128(int256(fee)), 0);
+
+        return (BaseHook.beforeSwap.selector, delta, 0);
+        // return (BaseHook.beforeSwap.selector, BeforeSwapDelta.wrap(0), 0);
+    }
+}
---
+++ test/TanguHook.t.sol
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {LPFeeLibrary} from "v4-core/src/libraries/LPFeeLibrary.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+
+import {IUniversalRouter} from "universal-router/contracts/interfaces/IUniversalRouter.sol";
+import {Commands} from "universal-router/contracts/libraries/Commands.sol";
+
+import {IV4Router} from "v4-periphery/src/interfaces/IV4Router.sol";
+import {LiquidityAmounts} from "v4-periphery/src/libraries/LiquidityAmounts.sol";
+import {IPermit2} from "v4-periphery/lib/permit2/src/interfaces/IPermit2.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {Actions} from "v4-periphery/src/libraries/Actions.sol";
+import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
+
+import {TanguHook} from "../src/TanguHook.sol";
+import {IAavePool} from "../src/interfaces/IAavePool.sol";
+
+contract TestTanguHook is Test, IERC721Receiver {
+    using SafeERC20 for IERC20;
+
+    IPositionManager constant positionManager = IPositionManager(0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e);
+    IPoolManager constant poolManager = IPoolManager(0x000000000004444c5dc75cB358380D2e3dE08A90);
+    IPermit2 constant permit2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+    IUniversalRouter constant universalRouter = IUniversalRouter(0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af);
+
+    IAavePool constant aavePool = IAavePool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
+    IERC20 constant aUSDC = IERC20(0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c);
+    IERC20 constant aUSDT = IERC20(0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a);
+    IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20 constant USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+
+    TanguHook tanguHook;
+    PoolKey poolKey;
+    uint160 initSqrtPriceX96;
+
+    function onERC721Received(address, address, uint256, bytes calldata) external returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    function setupPool(address usdc, address usdt) public {
+        (, bytes32 salt) = HookMiner.find(
+            address(this),
+            uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_INITIALIZE_FLAG),
+            type(TanguHook).creationCode,
+            abi.encode(address(poolManager), address(aavePool))
+        );
+
+        tanguHook = new TanguHook{salt: salt}(poolManager, aavePool);
+
+        console2.log("tanguHook:", address(tanguHook));
+
+        tanguHook.setATokens(address(aUSDC), Currency.wrap(usdc));
+        tanguHook.setATokens(address(aUSDT), Currency.wrap(usdt));
+
+        // Create pool key for USDC/USDT
+        poolKey = PoolKey({
+            currency0: Currency.wrap(usdc),
+            currency1: Currency.wrap(usdt),
+            fee: 10000, // Must be 0 for wrapper pools
+            tickSpacing: 200,
+            hooks: IHooks(address(tanguHook))
+        });
+
+        initSqrtPriceX96 = uint160(TickMath.getSqrtPriceAtTick(0));
+        poolManager.initialize(poolKey, initSqrtPriceX96);
+
+        // Add liquidity
+        deal(address(USDC), address(this), 1_000_000e6);
+        deal(address(USDT), address(this), 1_000_000e6);
+        USDC.forceApprove(address(permit2), type(uint).max);
+        USDT.forceApprove(address(permit2), type(uint).max);
+        permit2.approve(address(USDC), address(positionManager), type(uint160).max, uint48(block.timestamp + 60));
+        permit2.approve(address(USDT), address(positionManager), type(uint160).max, uint48(block.timestamp + 60));
+
+        bytes memory actions = abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR));
+        bytes[] memory params = new bytes[](2);
+        params[0] = abi.encode(poolKey, -200, 200, 1_000_000e6, type(uint).max, type(uint).max, address(this), "");
+        params[1] = abi.encode(Currency.wrap(address(USDC)), Currency.wrap(address(USDT)));
+        positionManager.modifyLiquidities(abi.encode(actions, params), block.timestamp + 60);
+    }
+
+    function swapExactInputSingle(
+        PoolKey memory key,
+        uint128 amountIn,
+        uint128 minAmountOut
+    ) public returns (uint256 amountOut) {
+        // Encode the Universal Router command
+        bytes memory commands = abi.encodePacked(uint8(Commands.V4_SWAP));
+        bytes[] memory inputs = new bytes[](1);
+
+        // Encode V4Router actions
+        bytes memory actions = abi.encodePacked(
+            uint8(Actions.SWAP_EXACT_IN_SINGLE),
+            uint8(Actions.SETTLE_ALL),
+            uint8(Actions.TAKE_ALL)
+        );
+
+        // Prepare parameters for each action
+        bytes[] memory params = new bytes[](3);
+        params[0] = abi.encode(
+            IV4Router.ExactInputSingleParams({
+                poolKey: key,
+                zeroForOne: true,
+                amountIn: amountIn,
+                amountOutMinimum: minAmountOut,
+                hookData: bytes("")
+            })
+        );
+        params[1] = abi.encode(key.currency0, amountIn);
+        params[2] = abi.encode(key.currency1, minAmountOut);
+
+        // Combine actions and params into inputs
+        inputs[0] = abi.encode(actions, params);
+
+        // Execute the swap
+        universalRouter.execute(commands, inputs, block.timestamp + 60);
+
+        // Verify and return the output amount
+        amountOut = IERC20(Currency.unwrap(key.currency1)).balanceOf(address(this));
+        require(amountOut >= minAmountOut, "Insufficient output amount");
+        return amountOut;
+    }
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+    }
+
+    function testSetupPool() public {
+        setupPool(address(USDC), address(USDT));
+    }
+
+    function testHookDeployment() public {
+        setupPool(address(USDC), address(USDT));
+        PoolKey memory invalidPoolKey = PoolKey({
+            currency0: Currency.wrap(address(aUSDC)),
+            currency1: Currency.wrap(address(USDT)),
+            fee: 10000, // Must be 0 for wrapper pools
+            tickSpacing: 200,
+            hooks: IHooks(address(tanguHook))
+        });
+
+        vm.expectRevert();
+        poolManager.initialize(invalidPoolKey, initSqrtPriceX96);
+
+        invalidPoolKey = PoolKey({
+            currency0: Currency.wrap(address(aUSDC)),
+            currency1: Currency.wrap(address(USDC)),
+            fee: 10000, // Must be 0 for wrapper pools
+            tickSpacing: 200,
+            hooks: IHooks(address(tanguHook))
+        });
+        vm.expectRevert();
+        poolManager.initialize(invalidPoolKey, initSqrtPriceX96);
+    }
+
+    function testSwapFeeToAave() public {
+        setupPool(address(USDC), address(USDT));
+
+        USDC.approve(address(permit2), type(uint256).max);
+        permit2.approve(address(USDC), address(universalRouter), type(uint160).max, uint48(block.timestamp + 60));
+
+        swapExactInputSingle(poolKey, 1_000e6, 0);
+    }
+}
